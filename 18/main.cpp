07515b6153c32e0cf4bb606215245187b96fcec8
FilipKochan <filda.koch@gmail.com>
2022-12-18 22:40:46 +0100

    day 18 - pt 2

--- 18/main.cpp
+++ 18/main.cpp
@@ -7,11 +7,6 @@ struct point {
     ll x;
     ll y;
     ll z;
-
-    bool touches(point other) const {
-        return (std::abs(x + other.x) + std::abs(y + other.y) + std::abs(z + other.z)) == 1;
-    }
-
     void generate_possible(std::vector<point> *res) {
         res->push_back({x + 1, y, z});
         res->push_back({x - 1, y, z});
@@ -26,24 +21,79 @@ inline bool operator<(const point& l, const point& r)
 {
     return l.x < r.x || ((l.x == r.x) && (l.y < r.y)) || ((l.x == r.x) && (l.y == r.y) && l.z < r.z);
 }
-
+bool traverse(point current, std::set<point> *traversed, std::set<point> *points,
+              ll minx, ll miny, ll minz, ll maxx, ll maxy, ll maxz);
 int main() {
-    ll x, y, z;
+    ll x, y, z, minx = LLONG_MAX, miny = LLONG_MAX, minz = LLONG_MAX, maxx = LLONG_MIN, maxy = LLONG_MIN, maxz = LLONG_MIN;
+
     ll surface = 0;
     std::set<point> points;
     std::vector<point> possible;
     while (scanf("%llu,%llu,%llu\n", &x, &y, &z) == 3) {
+        minx = std::min(minx, x); miny = std::min(miny, y); minz = std::min(minz, z);
+        maxx = std::max(maxx, x); maxy = std::max(maxy, y); maxz = std::max(maxz, z);
+
         point current = {x, y, z};
         current.generate_possible(&possible);
         ll touching = 0;
+        for (auto p : possible) { touching += (ll)points.count(p); }
+        surface += 6 - touching * 2;
+        points.insert(current);
+        possible.clear();
+    }
+    std::cout << "part 1: " << surface << std::endl;
+
+    std::set<point> visited;
+    std::vector<point> gaps;
+    std::set<point> traversed;
+    for (ll i = minx + 1; i < maxx; i++) {
+        for (ll j = miny + 1; j < maxy; j++) {
+            for (ll k = minz + 1; k < maxz; k++) {
+                point current = {i, j, k};
+                if (visited.count(current) || points.count(current)) {
+                    continue;
+                }
+                bool res = traverse(current, &traversed, &points, minx, miny, minz, maxx, maxy, maxz);
+                for (auto  pp : traversed) {
+                    if (res) { gaps.push_back(pp); }
+                    visited.insert(pp);
+                }
+                traversed.clear();
+            }
+        }
+    }
+
+    for (auto current : gaps) {
+        current.generate_possible(&possible);
+        ll touching = 0;
         for (auto p : possible) {
-            touching += points.count(p);
+            touching += (ll)points.count(p);
         }
         surface += 6 - touching * 2;
         points.insert(current);
         possible.clear();
     }
 
-    std::cout << "part 1: " << surface << std::endl;
+    std::cout << "part 2: " << surface << std::endl;
     return 0;
+}
+
+
+bool
+traverse(point current, std::set<point> *traversed, std::set<point> *points, ll minx, ll miny,
+         ll minz, ll maxx, ll maxy, ll maxz) {
+    if (traversed->count(current) || points->count(current)) {
+        return true;
+    }
+    if (current.x <= minx || current.x >= maxx || current.y <= miny ||
+        current.y >= maxy || current.z <= minz || current.z >= maxz) {
+        return false;
+    }
+    traversed->insert(current);
+    std::vector<point> options = {};
+    current.generate_possible(&options);
+    return std::all_of(options.begin(), options.end(),
+                       [&traversed, &points, &minx, &miny, &minz, &maxx, &maxy, &maxz] (point p) {
+        return traverse(p, traversed, points, minx, miny, minz, maxx, maxy, maxz);
+    });
 }
